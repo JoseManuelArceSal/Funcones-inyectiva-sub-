29bcf5204c790e609e7a310979edd78fd857c29a
Jose Manuel Arce <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-11-16 17:30:47 -0600

    Add files via upload

---
+++ grafico1.m
@@ -0,0 +1,17 @@
+% Script Octave
+% Title           :Script para graficar funciones
+% Author          :Jose Manuel Arce Salvador
+% Date            :20211028
+% Version         :1
+% Usage           :octave
+%                 :Requiere aplicación octave para utilizar su linea de comandos
+
+% funcion g(x) = x^2
+x=-50:05:50
+% funcion a plotear
+fx = (x.^2);
+
+% Dibujar funcion
+plot(x,fx)
+
+
